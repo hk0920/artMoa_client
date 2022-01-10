42d57a13910676c5297d5583efc2a0cc005444d1
kao-victor <94291819+kao-victor@users.noreply.github.com>
2022-01-10 22:28:41 +0900

    Update setupProxy.js
    apply httpApi

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,16 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app){
 	app.use(
+		"/httpApi" ,
+		createProxyMiddleware({
+			target:"http://61.102.114.235:19090/",
+			changeOrigin:true,
+			pathRewrite:{
+				'^/httpApi':''
+			}
+		})
+	)
+	app.use(
 		"/artApi" ,
 		createProxyMiddleware({
 			target:"http://www.culture.go.kr/",
