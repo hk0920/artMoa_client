1fcdbfe0233bae13c2e136651c3be2ce396f95ef
hk0920 <gmlrb920@naver.com>
2022-02-08 18:32:07 +0900

    sub visual load motion file upload
    sub-visual background scale motion

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import $ from "jquery";
 
 const ScrollToTop=()=>{
 	const {pathname} = useLocation();
 
 	useEffect(()=>{
 		window.scrollTo(0, 0);
+		setTimeout(() => {
+			if($(".sub-vis").length > 0) {
+				$(".sub-vis").addClass("active");
+			}
+		}, 10);
 	},[pathname]);
 
 	return null;
